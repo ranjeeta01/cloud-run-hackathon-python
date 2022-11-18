f5fe67b64b72bed6a53acfae33ca9570bc59f4b2
Ranjeeta <41211457+ranjeeta01@users.noreply.github.com>
2022-11-18 08:15:24 +0000

    attack directions testing

--- main.py
+++ main.py
@@ -23,18 +23,65 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
+direction = ['N','S','E','W']
 
 @app.route("/", methods=['GET'])
 def index():
     return "Let the battle begin!"
 
+
+
+def checkNearestPlayer():
+
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
     logger.info(request.json)
+
+    size = request.json['arena']['dims'] #dimensions of arena
+    bots_state = request.json['arena']['state'] #state of the bots in arena
+    mybot_link = data["_links"]["self"]["href"] # my bot link
+
+    # {'x': 0, 'y': 0, 'direction': 'S', 'wasHit': False, 'score': 0} player response
+    mybot = players_state[mybot_link] # to get own bot state
+    amIHit = mybot["wasHit"]
+    #bots_state.pop(bots_state) to remove my state from the dic
     
+    possible_moves{
+        'S': {
+            'attack_moves': [[x, y-1], [x, y-2], [x, y-3]],
+        }, 
+        'N': {
+            'attack_moves': [[x, y+1], [x, y+2], [x, y+3]],
+        },
+        'E': {
+            'attack_moves': [[x+1, y], [x+2, y], [x+3, y]],
+        },
+        'W': {
+            'attack_moves': [[x-1, y], [x-2, y], [x-3, y]],
+        },  
+    }
+
     # TODO add your implementation here to replace the random response
     
+    for player in bots_state:
+        x,y=bots_state[player]
+        dirc = bots_state[player]['direction']
+
+        if mybot['x']==x and [x,abs(y-mybot['y'])] in possible_moves[mybot['direction']['attack_moves']]: #if player is in same row and attack dist
+            if mybot['direction']==direc:
+                return 'T'
+            else:
+                mybot['direction']=direc
+                return 'T'
+        
+        if mybot['x']==x and [x,abs(y+mybot['y'])] in possible_moves[mybot['direction']['attack_moves']]: #if player is in same row opp direc and attack dist
+            if mybot['direction']==direc:
+                return 'T'
+            else:
+                mybot['direction']=direc
+                return 'T'
+
     return moves[random.randrange(len(moves))]
 
 if __name__ == "__main__":
